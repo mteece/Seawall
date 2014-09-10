b999ba47044b187e9bd7a556b8c451d2831b2e25
Matthew Teece <mteece@pixelmedia.com>
2014-09-09 23:02:11 -0400

    Flushing cookies and fetching incognito window.

--- events.js
+++ events.js
@@ -14,11 +14,31 @@ function browserActionClickEvent (tab) {
     injectedMethod(tab, 'browserActionClickEvent', function (response) {
             var uri = response.data;
             if(uri){
-                chrome.windows.create({url: "http://"+uri, type: "normal", incognito: true, focused: true}, function(window){
-                    // Callback for newly created window.
-                });
+                flush(uri);
             }
             return true;
+    });
+}
+
+/*
+* Functions.
+*/
+function browserWindow (uri) {
+    chrome.windows.create({url: "http://"+uri, type: "normal", incognito: true, focused: true}, function(window){
+        // todo window
+    });
+
+    return true;
+}
+
+function flush (uri){
+    chrome.cookies.getAll({url: "http://www.seacoastonline.com"}, function(cookies) {
+        for (var i = 0; i < cookies.length; i++) {
+            chrome.cookies.remove({url: cookies[i].domain, name: cookies[i].name}, function(details) {
+                //todo details
+            });
+        }
+        browserWindow(uri);
     });
 }
 
--- injection.js
+++ injection.js
@@ -29,4 +29,3 @@ var injection = injection || (function(){
 })();
 
 
-
